7d5698c2aa29f95012916baeb4bafafc3322e726
unknown <milkica21422@gmail.com>
2023-02-24 21:54:02 +0100

    jao meni

---
+++ cypress/page_objects/addOrgPage.js
@@ -0,0 +1,34 @@
+class AddOrgPage{
+
+    get addOrgLink(){
+       return cy.get("/my-organizations");
+    }
+
+    get addOrgPageHeading(){
+        return cy.get("h2").eq(2);
+    }
+
+    get organizationInput(){
+        return this.createOrgModal.find("input")
+    }
+
+    get cancelBtn(){
+        return this.createOrgModal.find("button").eq(1)
+    }
+
+    get nextBtn(){
+        return this.createOrgModal.find("button").last()
+    }
+
+   get createOrgModal() {
+    return cy.get(".vs-c-modal");
+   }
+
+   createOrganization(title) {
+    this.organizationInput.type(title)
+    this.nextBtn.click()
+    this.nextBtn.click()
+   }
+}
+
+export const addOrgPage= new AddOrgPage();
